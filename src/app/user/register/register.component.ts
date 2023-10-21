38e5e9e32a7c39917d6a7b7c1e6192623ae71661
Celmad <manuelalaminosd@protonmail.com>
2023-10-21 13:08:30 +0200

    Form submission and submission alert added

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,6 +7,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  showAlert = false
+  alertMessage = 'Please wait! Your account is being created.'
+  alertColour = 'blue'
+
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -29,7 +33,9 @@ export class RegisterComponent {
     ]),
   });
 
-  constructor() {
-    this.registerForm.controls.name;
+  register(): void {
+    this.showAlert = true
+    this.alertMessage = 'Please wait! Your account is being created.'
+    this.alertColour = 'blue'
   }
 }
